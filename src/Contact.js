0de5648c96ab13538bb998656b9b4d1a73baf5be
Artan <artanpl03@gmail.com>
2022-07-28 23:25:17 -0400

    added Contact component

---
+++ src/Contact.js
@@ -0,0 +1,28 @@
+import './Contact.css'
+
+
+const Contact = () =>{
+
+    return(
+        <section className="contact__container" id="contact">
+        <h1 className="contact__title--small">Contact</h1>
+        <h2 className="contact__title--big">Get In Touch</h2>
+        <p className="contact__text">
+          Currently available for hire, my inbox is always open. Also avaiable
+          for freelance work.
+        </p>
+        <div className="contact__btn-container">
+          <a
+            className="contact__btn"
+            href="mailto:artanpl03@gmail.com"
+            rel="noreferrer"
+          >
+            <i className="fa-solid fa-envelope contact__btn-icon"></i>
+            Contact
+          </a>
+        </div>
+      </section>
+    )
+}
+
+export default Contact
--- src/Intro.js
+++ src/Intro.js
@@ -1,10 +1,20 @@
 import React, { useEffect, useRef } from "react";
 import Typed from "typed.js";
-import ProjectOne from "./ProjectOne";
 import "./Intro.css"
+import Contact from "./Contact";
 
 const Intro = () => {
-  
+  const ref = useRef(null)
+  const handleClick = () => {
+    ref.current?.scrollIntoView({behavior: 'smooth'});
+  };
+
+  // const ref1 = useRef(null)
+  // const handleAbout = () => {
+  //   ref1.current?.scrollIntoView({behavior: 'smooth'});
+  // };
+
+ 
 
     const typedRef = useRef();
 
@@ -25,17 +35,17 @@ const Intro = () => {
     <div>
        <div>
        <ul>
-       <li>
-            <a className="first" href="#about">Contact</a>
+            <li>
+            <a  className="first" href="#contact">Contact</a>
             </li>
             <li>
-                <a className="first" href="#contact">Portfolio</a>
+                <a onClick={handleClick} className="first" href="#portfolio">Portfolio</a>
             </li>
             <li>
-                <a className="first" href="#news">About</a>
+                <a className="first" href="#about">About</a>
             </li>
             <li>
-                <a className="first" id="active" href="#home">
+              <a  className="first" id="active" href="#home">
                   Home
                 </a>
             </li>
@@ -48,10 +58,8 @@ const Intro = () => {
           I'm a <span ref={typedRef}></span> Developer
         </h2>
         <p className="hero__text">
-          A Software Engineer who can build a web presence from the ground up
-          using current best practices. Passionate learner, hard worker, and
-          team player who is proficient in an array of scripting languages and
-          multimedia web tools.
+          A Software Engineer with a strong interest in building scalable and user-friendly applications. Proven track record of leading  projects from the ground up. Comfortable working in a team-oriented environment on both small and large projects.
+          
         </p>
         <div className="hero__btn">
           <a
@@ -75,87 +83,71 @@ const Intro = () => {
         </div>
       </section>
       <section className="about__container" id="about">
-        <h1 className="about__title">About Me</h1>
-        <div className="about__content">
+        <h1  className="about__title">About Me</h1>
+        <div  className="about__content">
           <div className="about__left">
             <p className="about__text">
-              Hello! my name is Jerome, a software engineer in New York. My
-              passion for web development sparked in 2018 when I completed my
-              first course with Codecademy Programming with Python. Completing
-              that course sparked a passion for learning and growth. I also
-              finished Udacity course Intro to Programming Nanodegree, three
-              Udemy courses such as The Web Developer Bootcamp, Build Responsive
-              Real-World Websites with HTML and CSS, and Modern React with
-              Redux. In the future, I see myself as a full-stack engineer
-              supporting clients and building products that will give back to
-              communities.
+            Hello! My name is Artan and I am a software engineer in New York. I received my introduction to software development during my involvement in creating an online tutoring company, which ignited a strong desire to learn more about the field. This led to my participation in General Assembly's SEI program, where I accrued over 480 hours of class time. When I am not  coding, you can expect to find me playing volleyball or exploring museums.
             </p>
 
-            <p className="about__text">
-              Currently, I am enrolled in the General Assembly as a part-time
-              software engineer immersive, expanding my learning and best
-              practices. When I am not in class or freelancing, you can find me
-              on twitch playing games such as Valorant, League of Legends, or
-              Lost Ark. Also a fan of basketball, reading, and working out.
-            </p>
+            
 
-            {/* <div className="about__tech">
-              <h2 className="about__tech-title">Tech</h2>
-              <ul className="about__tech-list">
-                {tech.map((item) => {
-                  return <AboutList id={item.id} tech={item.tech} />;
-                })}
-              </ul>
-            </div> */}
+            
           </div>
           <div className="about__right">
             <img src={'https://media-exp1.licdn.com/dms/image/C4E03AQGZKEIX7tNw7Q/profile-displayphoto-shrink_400_400/0/1637545247385?e=1664409600&v=beta&t=VRtbdj6ONhyJT7DfTDvIWkUhymEqziWEXLOjBvBSgsU'} alt="jerome" className="about__image" />
           </div>
         </div>
       </section>
-      <section>
-        <h1>Portfolio</h1>
-        <div className="project__div">
+      <section ref={ref}>
+        <h1  className="portfolio">Portfolio</h1>
+        <div   className="project__div">
           <div className="project__div__inside">
+
+
              <a
               href="https://open-vault-game.netlify.app/"
-              target="_blank"
-            >
+              target="_blank">
             <div className="project__one">
             <p className="open_vault">Open the Vault</p>
             <div className="space__around"></div>
             <p className="portfolio__text">
             Open the vault is a two player game, where one player picks a word and the other has 1 minute to guess it.
           </p>
-
             </div>
             </a>
+            
            
+
+             <a
+              href="https://artanscrypto.netlify.app/"
+              target="_blank">
             <div className="project__two">
-            CryptoSearch
-             <a
-
-              href="https://github.com/dev-rome/Tenant-Estate"
-              target="_blank"
-            >
-              
-
-            </a>
+            <p className="open_vault">CryptoSearch</p>
+            <div className="space__around"></div>
+            <p className="portfolio__text">
+            CryptoSearch lets you research crypto coins by marketcap, price and volume
+          </p>
             </div>
+            </a>
+            
+
+             <a
+              href="https://reveal-store.surge.sh/"
+              target="_blank">
             <div className="project__three">
-            Reveal
-             <a
-
-              href="https://github.com/dev-rome/Tenant-Estate"
-              target="_blank"
-            >
-              
-
-            </a>
+            <p className="open_vault">Reveal</p>
+            <div className="space__around"></div>
+            <p className="portfolio__text">
+            Online store lets users buy products 
+          </p>
             </div>
+            </a>
           </div>
         </div>
       </section>
+      <Contact />
+     
       
       </div>
   );
