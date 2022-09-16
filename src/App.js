a7e858ef541534a16c648bc7df4e0c0fc3daf032
Artan <artanpl03@gmail.com>
2022-09-15 20:18:44 -0400

    testing google analytics

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 
 import './App.css';
 import Intro from './Intro.js';
+import ReactGA from 'react-ga';
+import React, {useEffect} from 'react'
 
-import React, { useEffect, useRef } from "react";
 
 function App() {
-  const ref = useRef(null)
-  const handleClick = () => {
-    ref.current?.scrollIntoView({behavior: 'smooth'});
-  };
+  // const ref = useRef(null)
+  // const handleClick = () => {
+  //   ref.current?.scrollIntoView({behavior: 'smooth'});
+  // };
 
+  useEffect(() => {
+    ReactGA.initialize('GA-ID');
+    ReactGA.pageview(window.location.pathname + window.location.search)
+    },[]);
 
   return (
     <div className="App">
